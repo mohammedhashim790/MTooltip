dca2588bab782ef59f08a5d2206ff6e4bc7e6ad9
Mohammed Hashim <mohammedhashim790@gmail.com>
2025-11-02 22:01:14 -0400

    feat: Example for Android Device

---
+++ example/android/app/src/main/kotlin/com/example/mtooltip_example/mtooltip_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mtooltip_example.mtooltip_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ example/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:mtooltip/mtooltip.dart';
+import 'package:mtooltip/tooltip_custom_shape.dart';
+
+void main() {
+  runApp(const MyTooltipApp());
+}
+
+class MyTooltipApp extends StatelessWidget {
+  const MyTooltipApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MTooltip Example',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const TooltipExample(title: 'MTooltip Example'),
+    );
+  }
+}
+
+class TooltipExample extends StatefulWidget {
+  const TooltipExample({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<TooltipExample> createState() => _TooltipExampleState();
+}
+
+class _TooltipExampleState extends State<TooltipExample> {
+  GlobalKey<MTooltipState> state = GlobalKey<MTooltipState>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: IntrinsicHeight(
+          child: Column(
+            children: [
+              TextButton(
+                onPressed: () {
+                  // MTooltip.concealAll();
+
+                  state.currentState?.show();
+                },
+                child: const Text("Click to Show Bottom"),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: MTooltip(
+                  key: state,
+                  context: context,
+                  tooltipContent: const Text("This is a text"),
+                  backgroundColor: Colors.green,
+                  tooltipAlign: TooltipAlign.top,
+                  child: const Text("This is a text 123"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

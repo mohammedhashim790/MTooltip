11da2ec8e8af60d589fa00de4c0dd73601058050
Mohammed Hashim <mohammedhashim790@gmail.com>
2025-11-13 22:09:48 -0400

    chore: Flutter Docs update

--- lib/controller/mtooltip_controller.dart
+++ lib/controller/mtooltip_controller.dart
@@ -1,12 +1,37 @@
 import 'package:mtooltip/controller/mtooltip_controller_impl.dart';
 import 'package:mtooltip/mtooltip.dart';
 
+/// Controller interface for driving an [MTooltip] instance.
+///
+/// Implementations of this interface can be attached to an [MTooltipState]
+/// and provide a simple programmatic API to show or remove the tooltip.
+///
+/// Usage:
+/// ```dart
+/// final controller = MTooltipController(); // returns IMTooltipController
+/// // Provide `controller` to an MTooltip instance.
+/// controller.show();
+/// controller.remove();
+/// ```
 abstract class MTooltipController {
+  /// Show the attached tooltip.
+  ///
+  /// If no [MTooltipState] is attached this call is a no-op.
   void show();
 
+  /// Remove / dismiss the attached tooltip immediately.
+  ///
+  /// If no [MTooltipState] is attached this call is a no-op.
   void remove();
 
+  /// Attach a live [MTooltipState] to this controller.
+  ///
+  /// The controller will forward subsequent [show] and [remove] calls to the
+  /// provided state instance.
   void attach(MTooltipState widget);
 
+  /// Factory constructor that returns the default implementation.
+  ///
+  /// Currently returns an instance of [IMTooltipController].
   factory MTooltipController() => IMTooltipController();
 }
--- lib/controller/mtooltip_controller_impl.dart
+++ lib/controller/mtooltip_controller_impl.dart
@@ -1,21 +1,40 @@
 import 'package:mtooltip/controller/mtooltip_controller.dart';
 import 'package:mtooltip/mtooltip.dart';
 
+/// Concrete implementation of [MTooltipController].
+///
+/// This controller is intended to be provided to the [MTooltip] widget and
+/// attached to the widget's internal state. Calls to [show] and [remove]
+/// are forwarded to the attached [MTooltipState]. If no state is attached
+/// the methods are no-ops.
 class IMTooltipController implements MTooltipController {
+  IMTooltipController();
+
+  MTooltipState? _state;
+
+  /// Attach the controller to the live [MTooltipState].
+  ///
+  /// The controller will forward subsequent [show] and [remove] calls to the
+  /// provided state instance.
   @override
-  void remove() {
-    _state?.dismiss();
+  void attach(MTooltipState state) {
+    _state = state;
   }
 
+  /// Request the tooltip to be shown.
+  ///
+  /// If no [MTooltipState] is attached this method does nothing.
   @override
   void show() {
     _state?.show();
   }
 
-  MTooltipState? _state;
-
+  /// Request the tooltip to be removed/dismissed.
+  ///
+  /// Delegates to the attached state's [dismiss] method. If no state is
+  /// attached this method is a no-op.
   @override
-  void attach(MTooltipState state) {
-    _state = state;
+  void remove() {
+    _state?.dismiss();
   }
 }
